3cb5887fe24c7ed51c44911687f4a9c69e5b6b7c
Kwespo <73428848+Kwespo@users.noreply.github.com>
2023-03-11 00:13:00 +0000

    1st

---
+++ File handling/main.py
@@ -0,0 +1,8 @@
+inp = input("")
+f = open("File handling\example.txt" , "r+") #opens the file (r+ allows us to write in it too)
+print(f.read()) #reads and print out the entire file
+
+f.write(f"\n" + inp) #makes a new line and adds whatever the input was.
+
+f.close()
+
